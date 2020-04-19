17f703d154158e5cf0b10ac80a72b2cdabb0ae14
Sam Neal <sam@nealblim.com>
2020-04-19 12:56:09 -0400

    Added header, first working version commit

--- server.js
+++ server.js
@@ -1,61 +1,83 @@
-var flash = require('flash');
-var express = require('express');
-var session = require('express-session');
+/* ╔══════════════════════════════════════╦═════════════════════════╦══════════╗
+ * ║ server.js                            ║ Created:   16 Dec. 2020 ║ v1.0.0.1 ║
+ * ║ (part of robot.nealblim.com user     ║ Last mod.: 19 Apr. 2020 ╚══════════╣
+ * ║ authorization/web server)            ║                                    ║
+ * ╠══════════════════════════════════════╩════════════════════════════════════╣
+ * ║ Entry point for this Node.js application.                                 ║
+ * ║ Used for authenticating users based on login credentials stored in a      ║
+ * ║ MySQL database, using Express, Passport, Sequelize and more.              ║
+ * ╠═══════════════════════════════════════════════════════════════════════════╣
+ * ║ For the latest version of field.js, to report a bug, or to contribute,    ║ 
+ * ║ visit:     github.com/snealbli/                                           ║
+ * ║    or:     robot.nealblim.com/                                            ║
+ * ╠═══════════════════════════════════════════════════════════════════════════╣
+ * ║                         by Samuel 'teer' Neal-Blim                        ║
+ * ║                                                                           ║
+ * ║                          Site: prog.nealblim.com                          ║
+ * ║                         Git:   github.com/snealbli                        ║
+ * ║                     JSfiddle:  jsfiddle.net/user/teeer                    ║
+ * ╚═══════════════════════════════════════════════════════════════════════════╝
+ */
+const express                = require('express'),
+      expHandlebars          = require('express-handlebars'),
+      session                = require('express-session'),
+      cookieParser           = require('cookie-parser'),
+      bodyParser             = require('body-parser'),
+                             
+      app                    = express(),
+      fs                     = require('fs'),
+      path                   = require('path'),
+      flash                  = require('connect-flash');
+      
+const passport               = require('passport'),
+      env                    = require('dotenv').config();
 
-var app = express();
+app.engine('handlebars',       require('express-handlebars')
+    .create({
+        extname:       '.handlebars', 
+        defaultLayout: 'main',
+        layoutsDir:    path.join(__dirname, '/views/layouts/'),
+        partialsDir:   path.join(__dirname, 'views/partials/')
+    }).engine
+);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/views'));
 
-var passport = require('passport');	/*, 
-	linkedinStrategy = require('passport-linkedin').Strategy,
-	githubStrategy = require('passport-github').Strategy,
-	facebookStrategy = require('passport-facebook').Strategy,
-	googleStrategy = require('passport-google').Strategy,
-	twitterStrategy = require('passport-twitter').Strategy,
-	stackExchangeStrategy = require('passport-stack-exchange').Strategy,
-	redditStrategy = require('passport-reddit').Strategy;*/
-var bodyParser = require('body-parser');
-var env = require('dotenv').config();
-
-app.use(express.static(__dirname + "/public/public_html"));
-
-//bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
+//BodyParser
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
 app.use(bodyParser.json());
 
-//passport
-app.use(passport.initialize());
-app.use(passport.session(
-		{ 
-			secret: 'its_a_secret', 
-			cookie: {
-				maxAge: 259200,	//30 days
-				secure: false
-			},
-			resave: false,
-			saveUninitialized: false
-})); // session secret
+//Passport
+app.use(session(
+        { 
+            secret:            'wOuLdN\'t_YoU_lIkE_t0_KnOW? ;)', 
+            cookie: {
+                maxAge:        259200,    //30 days
+                secure:        false
+            },
+            resave:            true,
+            saveUninitialized: true
+})); 
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 //Models
-var users = require("./app/user");
+var models = require('./app/models/');
 
 //Routes
 var authRoute = require('./app/routes/auth.js')(app, passport);
 
 //load passport strategies
-require('./app/config/passport.js')(passport, users.user);
- 
-//Sync Database
-users.sequelize.sync().then(function() {
-    console.log('Nice! Database looks fine');
-}).catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!");
-});
+require('./app/config/passport.js')(passport, models.user);
 
-app.listen(5000, function(err) {
+app.listen(9999, function(err) {
     if (!err) {
         console.log("Site is live");
     } else {
-    	console.log(err);
+        console.log(err);
     }
 });
